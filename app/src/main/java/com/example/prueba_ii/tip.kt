dae3f59dc6decafa9911f4990b3dfd0132f765f4
CursoApp <ojara@gmx.com>
2024-07-30 09:59:34 +0200

    1er comm

---
+++ app/src/main/java/com/example/prueba_ii/tip.kt
@@ -0,0 +1,11 @@
+package com.example.prueba_ii
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Tip(
+
+    val amount: Double,
+    val date: String,
+    val description: String
+)
